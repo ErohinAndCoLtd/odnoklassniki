71f6850892c3ca007095c66ccfc66d61f5a6b2e9
ErohinAndCoLtd <ErohinAndCoLtd@github.com>
2013-11-28 22:47:28 +0100

    Now for real

--- Disliker.user.js
+++ Disliker.user.js
@@ -17,7 +17,7 @@ var reloadWaitTime = 2000,
     minAge = "14",    // >= 14
     maxAge = "99";    // <= 99
 
-document.getElementsByClass = function(tagName, className) {
+document.getElementsByClass = function(tagName, className) {                                             // Вспомогательная функция, позволяющая получить все элементы с определенным тэгом и классом
   var itemsfound = new Array,
       elems = document.getElementsByTagName(tagName),
       i;
@@ -31,67 +31,66 @@ document.getElementsByClass = function(tagName, className) {
 
 function processPhoto (photos) {
     var evObj;
-    if (photos.length === 0) {
+    if (photos.length === 0) {                                                                            // Если длина буфера фото - ноль, значит, они кончились. Возвращаемся в начало скрипта.
       setTimeout(doScript, reloadWaitTime);
       return;
     }
     
-    evObj = document.createEvent('MouseEvents');
+    evObj = document.createEvent('MouseEvents');                                                           // Для вывода всплывающего меню симулируем ивент прохождения курсора мыши по фото
     evObj.initEvent('mouseover', true, false);
     photos[0].children[0].children[0].dispatchEvent(evObj);
     
-    setTimeout(function () {
-      var photoLink = document.getElementsByClass("a", "gwt-shortcutMenu-iconlink-item")[0].children[0];
-      photoLink.click();
+    setTimeout(function () {                                                                                // Ждем, пока меню всплывет и
+      var photoLink = document.getElementsByClass("a", "gwt-shortcutMenu-iconlink-item")[0].children[0];    // Получаем ссылку на увеличенное фото и
+      photoLink.click();                                                                                    // Жмем ее
     }, popupWaitTime);
     
-    setTimeout(function () {
-      var dislikeBlock = document.getElementsByClass("li", "mark mark__last")[0],
+    setTimeout(function () {                                                                                 // Ждем, пока откроется увеличенное фото
+      var dislikeBlock = document.getElementsByClass("li", "mark mark__last")[0],                            // Получаем блок с дизлайком
         dislikeButton,
-        closeButton = document.getElementsByClass("div", "ic ic_i_close")[0],
-        usersOnlineButton = document.getElementsByClass("a", "sm fo4c_h_live-link fs-15")[0];
+        closeButton = document.getElementsByClass("div", "ic ic_i_close")[0],                                // Получаем кнопку закрытия увеличенной фотографии
+        usersOnlineButton = document.getElementsByClass("a", "sm fo4c_h_live-link fs-15")[0];                // Получаем кнопку возврата к пользователям онлайн
         
-      photos.splice (0,1);
+      photos.splice (0,1);                                                                                   // Вне зависимости от результата, первая фотка в буфере нам уже не нужна
         
-      if (dislikeBlock) {
-        dislikeButton = dislikeBlock.children[0];
-        dislikeButton.click();
-        closeButton.click();
+      if (dislikeBlock) {                                                                                    // Если блок с дизлайком найден, то
+        dislikeButton = dislikeBlock.children[0];                                                            // Находим собственно кнопку
+        dislikeButton.click();                                                                               // Жмем
+        closeButton.click();                                                                                 // И закрываем увеличенное изображение
         setTimeout (function () {
-          processPhoto (photos);
+          processPhoto (photos);                                                                             // После чего переходим к следующему фото
         }, closeWaitTime);
       } else {
-         usersOnlineButton.click();
+         usersOnlineButton.click();                                                                          // Если же нет, то возвращаемся к странице с пользователями и перезапускаемся
          setTimeout(doScript, reloadWaitTime);
       }
     }, photoLoadWaitTime);
 }
 
 function fetchPhotos () {
-  var photos = document.getElementsByClass("a", "photoWrapper"),
-      usersOnlineButton = document.getElementsByClass("a", "sm fo4c_h_live-link fs-15")[0];
-  if (photos.length === 0) {
+  var photos = document.getElementsByClass("a", "photoWrapper"),                                // Находим все фото
+      usersOnlineButton = document.getElementsByClass("a", "sm fo4c_h_live-link fs-15")[0];     // Находим кнопку возврата на страницу "Пользователи онлайн"
+  if (photos.length === 0) {                                                                    // Если фото не нашлись, то возвращаемся к пользователям онлайн и повторяем
       usersOnlineButton.click();
       setTimeout(doScript, reloadWaitTime);
-  } else {
+  } else {                                                                                       // Если нашлись, запускаем обработку фото
     processPhoto (photos);
   }
 }
    
 function doScript() {
-  var photos = document.getElementsByClass("a", "photoWrapper"),
-      ageSelect = document.getElementsByClass("select", "isl isl__res isl__2num"),
+  var ageSelect = document.getElementsByClass("select", "isl isl__res isl__2num"),                // Находим селекторы возраста
       minAgeSelect = ageSelect[0],
       maxAgeSelect = ageSelect[1],
       checkbox;
-  
+                                                                                                  // Устанавливаем возрасты
   minAgeSelect.value = minAge;
   maxAgeSelect.value = maxAge;
   
-  checkbox = document.getElementById ("field_female");
-  checkbox.click ();
-  checkbox.click ();
-  setTimeout(fetchPhotos, fetchWaitTime);
+  checkbox = document.getElementById ("field_female");                                            // Выбираем чекбокс "женщины" и кликаем два раза.
+  checkbox.click ();                                                                              // Это нужно потому, что после изменения значени селекторов
+  checkbox.click ();                                                                              // программно перезагрузски фото не происходит
+  setTimeout(fetchPhotos, fetchWaitTime);                                                         // Запускаем загрузку фото
 }
 
 doScript();
